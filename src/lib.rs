fa6978510cf6ebf21e47a1521b96616d839dd4e2
YYWmus <88748057+YYWmus@users.noreply.github.com>
2024-07-31 17:07:55 -0400

    specification updates

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,15 @@ impl ObjIdTraits for i64 {}
 impl ObjIdTraits for i32 {}
 impl ObjIdTraits for String {}
 
-pub trait Cache <ObjId:ObjIdTraits> {
-    fn get_mr(&mut self,trace: dyn Iterator::<Item = ObjId>, cache_size: usize) -> f64;
+pub trait CacheSim <ObjId:ObjIdTraits> {
+    // fn set_trace(&mut self, trace: impl Iterator::<Item = ObjId>);
+
+    fn get_total_miss(&mut self, trace: impl Iterator::<Item = ObjId>, cache_size: usize) -> (usize, usize);
+    /// returns (total_access_count, miss_count)
+
+    fn get_mr(&mut self, trace: impl Iterator::<Item = ObjId>, cache_size: usize) -> f64 {
+        let (total, miss) = self.get_total_miss(trace, cache_size);
+        miss as f64 / total as f64
+    }
 }
 
