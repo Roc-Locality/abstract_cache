e3271ee202b0a6e35dcfe63970939dc829790503
YYWmus <88748057+YYWmus@users.noreply.github.com>
2024-08-01 15:16:36 -0400

    modified interface after meeting

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,29 @@ impl ObjIdTraits for i64 {}
 impl ObjIdTraits for i32 {}
 impl ObjIdTraits for String {}
 
+pub enum AccessResult {
+    Hit,
+    Miss
+}
 
 pub trait CacheSim <ObjId:ObjIdTraits> {
-    fn get_total_miss(&mut self, trace: impl Iterator::<Item = ObjId>, cache_size: usize) -> (usize, usize);
+    fn cache_access(&mut self, access: ObjId) -> AccessResult;
+
+    fn set_capacity(&mut self, cache_size:usize) -> &mut Self;
+
+    fn get_total_miss(&mut self, trace: impl Iterator::<Item = ObjId>) -> (usize, usize) {
+        trace.fold((0,0), |(mut total, mut miss), o| {
+            let access = self.cache_access(o);
+            total += 1;
+            miss = if let AccessResult::Miss = access {miss + 1} else {miss};
+            (total, miss)
+        })
+    }
+    
     /// returns (total_access_count, miss_count)
 
-    fn get_mr(&mut self, trace: impl Iterator::<Item = ObjId>, cache_size: usize) -> f64 {
-        let (total, miss) = self.get_total_miss(trace, cache_size);
+    fn get_mr(&mut self, trace: impl Iterator::<Item = ObjId>) -> f64 {
+        let (total, miss) = self.get_total_miss(trace);
         miss as f64 / total as f64
     }
 }
